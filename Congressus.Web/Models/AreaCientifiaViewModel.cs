e1143bcbb93858f373d5c5e835ef63e2b18bd4a5
FerSSD <FerSSD@DESKTOP-F9AD0TV>
2017-06-21 21:35:42 -0300

    .

--- Congressus.Web/Models/AreaCientifiaViewModel.cs
+++ Congressus.Web/Models/AreaCientifiaViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Congressus.Web.Models.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,8 +11,10 @@ namespace Congressus.Web.Models
     public class AreaCientifiaViewModel
     {
         public int Id { get; set; }
+        [Display(Name ="Descrpción")]
         public string Descripcion { get; set; }
         public int EventoId { get; set; }
+        [Display(Name = "Miembro de comité científico")]
         public int MiembroComiteId { get; set; }
         public IEnumerable<SelectListItem> Miembros { get; set; }
 
